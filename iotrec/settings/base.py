ba8ccef7b8704df0a165f04fc2d1db301a83478a
Alex <alex.ikas@tum.de>
2019-09-11 16:30:57 +0200

    Fix static files path && Update readme

--- iotrec/settings/base.py
+++ iotrec/settings/base.py
@@ -87,7 +87,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
