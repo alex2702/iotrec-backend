ed45cffbfaf843b977ef60dac6f4a2c58ea30654
Alex <alex.ikas@tum.de>
2019-10-17 12:18:07 +0200

    Add statistics

--- training/urls.py
+++ training/urls.py
@@ -5,7 +5,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('', views.add_sample, name='add_sample')
+    path('', views.add_sample, name='add_sample'),
+    path('statistics/', views.get_statistics, name='get_statistics')
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- training/views.py
+++ training/views.py
@@ -9,6 +9,23 @@ from training.forms import SampleForm
 from training.models import TrainingUser, ReferenceThing, ContextFactor, Sample
 
 
+def is_combination_unambiguous(thing, context_factor, context_factor_value):
+    # check if we already have an unambiguous influence on the given thing/context_factor/context_factor_value combination
+    samples = Sample.objects.filter(thing=thing, context_factor=context_factor,
+                                    context_factor_value=context_factor_value)
+    # we need at least 4 samples
+    if len(samples) >= 4:
+        # check if all values in the samples are identical
+        all_values_identical = True
+        value_of_sample_1 = samples[0].value
+        for sample in samples:
+            if sample.value != value_of_sample_1:
+                all_values_identical = False
+                break
+        return all_values_identical
+    return False
+
+
 def add_sample(request, context_factor=None):
     if request.method == "POST":
         form = SampleForm(request.POST)
@@ -59,7 +76,7 @@ def add_sample(request, context_factor=None):
             return redirect('add_sample')
         else:
             print(form.errors)
-            #return redirect('add_sample')
+            # return redirect('add_sample')
     else:
         # create user if needed
         if 'iotrec_training_user' in request.COOKIES:
@@ -73,6 +90,8 @@ def add_sample(request, context_factor=None):
 
         # get random thing, context factor and context factor value
         random_thing = random.choice(ReferenceThing.objects.filter(active=True))
+
+
         random_context_factor_1 = random.choice(ContextFactor.objects.filter(active=True))
         random_context_factor_2 = random.choice(ContextFactor.objects.filter(active=True)
                                                 .exclude(pk=random_context_factor_1.pk))
@@ -104,6 +123,118 @@ def add_sample(request, context_factor=None):
                                                       .exclude(pk=random_context_factor_value_2.pk)
                                                       .exclude(pk=random_context_factor_value_3.pk)
                                                       .exclude(pk=random_context_factor_value_4.pk))
+
+        '''
+        while True:
+            try:
+                random_context_factor_1 = random.choice(ContextFactor.objects.filter(active=True))
+                random_context_factor_value_1 = random.choice(random_context_factor_1.values.filter(active=True))
+
+                while is_combination_unambiguous(random_thing, random_context_factor_1, random_context_factor_value_1):
+                    random_context_factor_1 = random.choice(ContextFactor.objects.filter(active=True))
+                    random_context_factor_value_1 = random.choice(random_context_factor_1.values.filter(active=True))
+
+                print(random_context_factor_1)
+                print(random_context_factor_value_1)
+            except IndexError:
+                print(random_context_factor_1)
+                print(random_context_factor_value_1)
+                print("IndexError, retrying...")
+                continue
+            break
+
+        while True:
+            try:
+                random_context_factor_2 = random.choice(ContextFactor.objects.filter(active=True))
+                random_context_factor_value_2 = random.choice(random_context_factor_2.values.filter(active=True)
+                                                              .exclude(pk=random_context_factor_value_1.pk))
+
+                while is_combination_unambiguous(random_thing, random_context_factor_2, random_context_factor_value_2):
+                    random_context_factor_2 = random.choice(ContextFactor.objects.filter(active=True))
+                    random_context_factor_value_2 = random.choice(random_context_factor_2.values.filter(active=True)
+                                                                  .exclude(pk=random_context_factor_value_1.pk))
+
+                print(random_context_factor_2)
+                print(random_context_factor_value_2)
+            except IndexError:
+                print(random_context_factor_2)
+                print(random_context_factor_value_2)
+                print("IndexError, retrying...")
+                continue
+            break
+
+        while True:
+            try:
+                random_context_factor_3 = random.choice(ContextFactor.objects.filter(active=True))
+                random_context_factor_value_3 = random.choice(random_context_factor_3.values.filter(active=True)
+                                                              .exclude(pk=random_context_factor_value_1.pk)
+                                                              .exclude(pk=random_context_factor_value_2.pk))
+
+                while is_combination_unambiguous(random_thing, random_context_factor_3, random_context_factor_value_3):
+                    random_context_factor_3 = random.choice(ContextFactor.objects.filter(active=True))
+                    random_context_factor_value_3 = random.choice(random_context_factor_3.values.filter(active=True)
+                                                                  .exclude(pk=random_context_factor_value_1.pk)
+                                                                  .exclude(pk=random_context_factor_value_2.pk))
+
+                print(random_context_factor_3)
+                print(random_context_factor_value_3)
+            except IndexError:
+                print(random_context_factor_3)
+                print(random_context_factor_3)
+                print("IndexError, retrying...")
+                continue
+            break
+
+        while True:
+            try:
+                random_context_factor_4 = random.choice(ContextFactor.objects.filter(active=True))
+                random_context_factor_value_4 = random.choice(random_context_factor_4.values.filter(active=True)
+                                                              .exclude(pk=random_context_factor_value_1.pk)
+                                                              .exclude(pk=random_context_factor_value_2.pk)
+                                                              .exclude(pk=random_context_factor_value_3.pk))
+
+                while is_combination_unambiguous(random_thing, random_context_factor_4, random_context_factor_value_4):
+                    random_context_factor_4 = random.choice(ContextFactor.objects.filter(active=True))
+                    random_context_factor_value_4 = random.choice(random_context_factor_4.values.filter(active=True)
+                                                                  .exclude(pk=random_context_factor_value_1.pk)
+                                                                  .exclude(pk=random_context_factor_value_2.pk)
+                                                                  .exclude(pk=random_context_factor_value_3.pk))
+
+                print(random_context_factor_4)
+                print(random_context_factor_value_4)
+            except IndexError:
+                print(random_context_factor_4)
+                print(random_context_factor_4)
+                print("IndexError, retrying...")
+                continue
+            break
+
+        while True:
+            try:
+                random_context_factor_5 = random.choice(ContextFactor.objects.filter(active=True))
+                random_context_factor_value_5 = random.choice(random_context_factor_5.values.filter(active=True)
+                                                              .exclude(pk=random_context_factor_value_1.pk)
+                                                              .exclude(pk=random_context_factor_value_2.pk)
+                                                              .exclude(pk=random_context_factor_value_3.pk)
+                                                              .exclude(pk=random_context_factor_value_4.pk))
+
+                while is_combination_unambiguous(random_thing, random_context_factor_5, random_context_factor_value_5):
+                    random_context_factor_5 = random.choice(ContextFactor.objects.filter(active=True))
+                    random_context_factor_value_5 = random.choice(random_context_factor_5.values.filter(active=True)
+                                                                  .exclude(pk=random_context_factor_value_1.pk)
+                                                                  .exclude(pk=random_context_factor_value_2.pk)
+                                                                  .exclude(pk=random_context_factor_value_3.pk)
+                                                                  .exclude(pk=random_context_factor_value_4.pk))
+
+                print(random_context_factor_5)
+                print(random_context_factor_value_5)
+            except IndexError:
+                print(random_context_factor_5)
+                print(random_context_factor_5)
+                print("IndexError, retrying...")
+                continue
+            break
+        '''
 
         form = SampleForm(
             initial={
@@ -139,7 +270,32 @@ def add_sample(request, context_factor=None):
                           }
                           )
 
-        response.set_cookie(key='iotrec_training_user', value=str(user.identifier), expires=datetime.datetime(2020, 1, 1))
+        response.set_cookie(key='iotrec_training_user', value=str(user.identifier),
+                            expires=datetime.datetime(2020, 1, 1))
         return response
 
 
+def get_statistics(request):
+
+    unambiguous_counter = 0
+    total_counter = 0
+
+    # go through all possible combinations and check for unambiguousness
+    all_things = ReferenceThing.objects.all()
+    all_context_factors = ContextFactor.objects.all()
+
+    for thing in all_things:
+        for context_factor in all_context_factors:
+            cf_values = context_factor.values.all()
+            for cf_value in cf_values:
+                total_counter += 1
+                if is_combination_unambiguous(thing, context_factor, cf_value):
+                    unambiguous_counter += 1
+
+    response = render(request, 'statistics.html',
+                      {
+                            'total_counter': total_counter,
+                            'unambiguous_counter': unambiguous_counter
+                      })
+
+    return response
