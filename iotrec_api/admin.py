f1bc13e4ce69b02d09ee77d3e334bf6db7db0e28
Alex <alex.ikas@tum.de>
2019-11-16 15:57:55 +0100

    Add filters for admin

--- iotrec_api/admin.py
+++ iotrec_api/admin.py
@@ -368,9 +368,9 @@ admin.site.register(Venue, VenueAdmin)
 
 class RecommendationAdmin(admin.ModelAdmin):
     fields = ['id', 'user', 'thing', 'context', 'experiment', 'score', 'preference_score', 'context_score', 'invoke_rec', 'created_at', 'updated_at']
-    list_display = ('id', 'created_at', 'user', 'thing', 'score', 'preference_score', 'context_score', 'invoke_rec')
+    list_display = ('id', 'created_at', 'user', 'thing', 'score', 'preference_score', 'context_score', 'experiment', 'invoke_rec')
     ordering = ('-created_at',)
-    list_filter = ['user', 'thing', 'experiment', 'created_at']
+    list_filter = ['user', 'thing', 'experiment', 'created_at', 'invoke_rec']
 
     def get_readonly_fields(self, request, obj=None):
         return ['id', 'created_at', 'updated_at', 'score', 'preference_score', 'context_score', 'invoke_rec']
